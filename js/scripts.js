deeffaf2a533799c21d2e3749f016f27c6f350e2
Cody Fritz <clanalia55@gmail.com>
2020-06-19 12:13:29 -0700

    update progress, user logic basically finished, object and parameters defined, begin work on prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,20 @@
 $(document).ready(function(){
   $('form#orderPizza').submit(function(event) {
     event.preventDefault();
+    let clientName = $('.userName').val();
+    console.log(clientName)
+    let pizzaSize = $('input:radio[name=pizzaSize]:checked').val();
+    console.log(pizzaSize);
+    let toppings = [];
+    $('input:checkbox[name=toppings]:checked').each(function() {
+    const toppingsList = $(this).val();
+    toppings.push(toppingsList)
+    })
+    console.log(toppings)
+    let pizzaPie = new Pizza(pizzaSize, toppings)
+    console.log(pizzaPie)
+
+    $('.nameResult').text('Hello, ' + clientName + ', thank you for ordering!');
   })
 })
   ///////////////////////////
@@ -13,5 +27,18 @@ $(document).ready(function(){
 function Pizza(size, toppings, name) {
 this.size = size;
 this.toppings = toppings;
-this.userName = name;
 }
+
+///Brute Force Solution///
+
+// Pizza.prototype.calculate = function(size, toppings)
+// let smallSize = 5
+// let mediumSize = 6.5
+// let largeSize = 8
+// //
+// let pepperoni = 0.5
+// let sausage = 0.55
+// let bacon = 0.65
+// let steak = 0.75
+// let extraCheese = 0.4
+// let ham = 0.60
