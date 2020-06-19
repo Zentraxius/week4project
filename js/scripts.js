5db423fb3a41a5406c64d788e20ad5efbef37c51
Cody Fritz <clanalia55@gmail.com>
2020-06-19 13:01:01 -0700

    update JS, improved legibility, streamlined some code sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,56 @@
   /////////////////////////////
  /// User Interface Logic. ///
-///////////////////////////// 
-$(document).ready(function(){
-  $('form#orderPizza').submit(function(event) {
-    event.preventDefault();
-    let clientName = $('.userName').val();
-    console.log(clientName)
-    let pizzaSize = $('input:radio[name=pizzaSize]:checked').val();
-    console.log(pizzaSize);
-    let toppings = [];
-    $('input:checkbox[name=toppings]:checked').each(function() {
-    const toppingsList = $(this).val();
-    toppings.push(toppingsList)
-    })
-    console.log(toppings)
-    let pizzaPie = new Pizza(pizzaSize, toppings)
-    console.log(pizzaPie)
+/////////////////////////////
+$(document).ready(function () { // Wait until Ready
+  $("form#orderPizza").submit(function (event) { // Only activates on submit
+    event.preventDefault(); // Stops black magic
 
-    $('.nameResult').text('Hello, ' + clientName + ', thank you for ordering!');
-  })
-})
+    let clientName = $(".userName").val(); // Takes input and converts to a variable for use
+    console.log(clientName); // Debug 
+
+    let pizzaSize = $("input:radio[name=pizzaSize]:checked").val(); // Takes the size selection and converts to a variable
+    console.log(pizzaSize); // Debug
+
+    let toppings = []; // Creates empty Array for use below
+    $("input:checkbox[name=toppings]:checked").each(function () { // *simple* array/checkbox
+      const toppingsList = $(this).val(); // Just takes results of each checkbox 
+      toppings.push(toppingsList); // Pushes results into array
+    });// Somehow the simplest option I found to put each topping into the array.
+    console.log(toppings); // Debug
+
+    let pizzaPie = new Pizza(pizzaSize, toppings); // Takes created variables above and uses to make object using constructor Pizza
+    console.log(pizzaPie); // Debug
+
+$('.orderTotal').text(pizzaPie.calculate) // Calls calculate prototype
+    $(".nameResult").text("Hello, " + clientName + ", thank you for ordering!"); // Takes name, thanks name.
+  });
+});
+
   ///////////////////////////
  ////  Business Logic.  ////
 ///////////////////////////
 
-function Pizza(size, toppings, name) {
-this.size = size;
-this.toppings = toppings;
+function Pizza(size, toppings) { // Confusion begins
+  this.size = size; // Takes size parameter and assigns as size
+  this.toppings = toppings; // Takes toppings parameter and calls it toppings
 }
 
-///Brute Force Solution///
+///Brute Force Solution/// (note-failing)
 
-// Pizza.prototype.calculate = function(size, toppings)
-// let smallSize = 5
-// let mediumSize = 6.5
-// let largeSize = 8
-// //
-// let pepperoni = 0.5
-// let sausage = 0.55
-// let bacon = 0.65
-// let steak = 0.75
-// let extraCheese = 0.4
-// let ham = 0.60
+Pizza.prototype.calculate = function (size, toppings) {
+  let holding = size
+  let smallSize = 5;
+  let mediumSize = 6.5;
+  let largeSize = 8;
+  console.log(holding)
+  //
+  let pepperoni = 0.5;
+  let sausage = 0.55;
+  let bacon = 0.65;
+  let steak = 0.75;
+  let extraCheese = 0.4;
+  let ham = 0.6;
+  
+};
+// Take size parameter and convert into price for mediumSize, if small, convert to price
+// take array, loop through each object inside array and convert price 
