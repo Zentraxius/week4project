d7eed44e7be3cc4dc4f055fe746bcee54f377b24
Cody Fritz <clanalia55@gmail.com>
2020-06-26 16:41:09 -0700

    apply polish to improve appearance of project

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,24 @@
 /// User Interface Logic. ///
 /////////////////////////////
 $(document).ready(function () {
-  // Wait until Ready
   $("form#orderPizza").submit(function (event) {
-    // Only activates on submit
-    event.preventDefault(); // Stops black magic
+    event.preventDefault(); 
+    let clientName = $(".userName").val(); 
+    let pizzaSize = $("input:radio[name=pizzaSize]:checked").val(); 
+    let toppings = []; 
 
-    let clientName = $(".userName").val(); // Takes input and converts to a variable for us
-
-    let pizzaSize = $("input:radio[name=pizzaSize]:checked").val(); // Takes the size selection and converts to a variable
-
-    let toppings = []; // Creates empty Array for use below
     $("input:checkbox[name=toppings]:checked").each(function () {
-      // *simple* array-checkbox
-      const toppingsList = $(this).val(); // Just takes results of each checkbox
-      toppings.push(toppingsList); // Pushes results into array
-    }); // Somehow the simplest option I found to put each topping into the array.
+      const toppingsList = $(this).val(); 
+      toppings.push(toppingsList); 
+    }); 
 
-    let pizzaPie = new Pizza(pizzaSize, toppings); // Takes created variables above and uses to make object using constructor Pizza
-
+    let pizzaPie = new Pizza(pizzaSize, toppings);
     let resultHold = pizzaPie.calculate();
-    console.log(resultHold);
-    // console.log(resultHold + " this is result");
 
-    $(".nameResult").text("Hello, " + clientName + ", thank you for ordering!"); // Takes name, thanks name.
+    $(".nameResult").text("Hello, " + clientName + ", thank you for ordering!");
     $(".orderTotal").text(
       "$" + resultHold + " is your total, have a great day!"
-    ); // Posts order total
+    );
   });
 });
 
@@ -36,24 +28,20 @@ $(document).ready(function () {
 ///////////////////////////
 
 function Pizza(size, toppings) {
-  // Confusion begins
-  this.size = size; // Takes size parameter and assigns as size
-  this.toppings = toppings; // Takes toppings parameter and calls it toppings
+
+  this.size = size;
+  this.toppings = toppings;
 }
 
 Pizza.prototype.calculate = function () {
-  // The magic
-  let pizzaTotal = 0; // Hold this number
-  ////////////////////////////////////////
+  let pizzaTotal = 0;
   if (this.size === "smallSize") {
-    // Bite sized noms
-    pizzaTotal += 4.99; // If small adds 5
+    pizzaTotal += 4.99; 
   } else if (this.size === "mediumSize") {
-    pizzaTotal += 6.49; // If medium adds 6.5
+    pizzaTotal += 6.49;
   } else if (this.size === "largeSize") {
-    pizzaTotal += 7.99; // If large adds 8
+    pizzaTotal += 7.99;
   }
-  pizzaTotal += this.toppings.length * 0.5; // Replaced over 30 lines of code with this 1 line.  I'm kind of upset, I spent 4 hours on those 30 lines.
-  ////////////////////////////////////////
-  return pizzaTotal; // When prototype is finished being used, the returned value is the pizzaTotal variable
+  pizzaTotal += this.toppings.length * 0.5;
+  return pizzaTotal;
 };
